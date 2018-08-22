e775352190af48d638cd78f21f01ccf25493f5bb
John Jiang <johnjiang101@gmail.com>
2018-08-22 10:38:52 -0700

    altitude

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const arDrone = require('ar-drone');
 const client = arDrone.createClient();
 
+const pngStream = client.getPngStream();
+
+client.config('control:altitude_max', 3000); // 3m altitude
+
 client.takeoff();
+// client.on('navdata', console.log);
+// pngStream.on('data', console.log);
 
 client
-  .after(5000, function() {
+  .after(3000, function() {
     this.clockwise(0.5);
   })
   .after(3000, function() {
     this.animate('flipLeft', 15);
+    
   })
   .after(1000, function() {
     this.stop();
