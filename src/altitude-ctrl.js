62720905c76c5a878fee2402860c07fd0f1fc52a
zhonghai-pmn <zzuo@philly.com>
2018-08-22 11:02:45 -0700

    feat: now the drone will float at 2 meters alt

---
+++ src/altitude-ctrl.js
@@ -0,0 +1,33 @@
+const _ = require('lodash');
+
+let client;
+let max_alt = 2.0;
+const THRESH_HOLD = 0.7;
+
+const init = (drone, altitude = 2.0) => {
+  client = drone;
+  client.config('control:altitude_max', 3000);
+
+  client.on('navdata', data => {
+    let alt = _.get(data, 'demo.altitudeMeters', 0);
+    console.log('Altitude => ', alt);
+    if (alt < altitude) up(alt);
+    else down();
+  });
+}
+
+function up(alt) {
+  let inc = alt < max_alt * THRESH_HOLD ? 0.5 : 0.1;
+  client.up(inc)
+}
+
+function down() {
+  client.down(0.1);
+}
+
+// ==========================================
+const altCtrl = {
+  init,
+}
+
+module.exports = altCtrl;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 const arDrone = require('ar-drone');
 const client = arDrone.createClient();
 
-const pngStream = client.getPngStream();
+// const pngStream = client.getPngStream();
 
-client.config('control:altitude_max', 3000); // 3m altitude
+const altCtrl = require('./altitude-ctrl');
+
+altCtrl.init(client);
 
 client.takeoff();
-// client.on('navdata', console.log);
-// pngStream.on('data', console.log);
 
-client
-  .after(3000, function() {
-    this.clockwise(0.5);
-  })
-  .after(3000, function() {
-    this.animate('flipLeft', 15);
-    
-  })
-  .after(1000, function() {
-    this.stop();
-    this.land();
-  });
+// client
+  // .after(3000, function() {
+    // this.clockwise(0.5);
+  // })
+  // .after(3000, function() {
+    // this.animate('flipLeft', 15);
+  // })
+  // .after(1000, function() {
+    // this.stop();
+    // this.land();
+  // });
